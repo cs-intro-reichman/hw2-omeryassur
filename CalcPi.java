c3697e9b6fb1a1a3d2f2b919c4141e09c75969b7
Omer Yassur <omeryassur@Omers-MacBook-Air.local>
2024-11-21 23:50:58 +0200

    submit

--- CalcPi.java
+++ CalcPi.java
@@ -1,15 +1,25 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-		int numberOfTerms = Integer.parseInt(args[0]);
-
-		double sum = 1;
-
-		for (int i = 1; i < numberOfTerms; i++) {
-			sum = i % 2 == 1 ? (sum - (1 / (double) (1 + (i * 2)))) : (sum + (1 / (double) (1 + (i * 2))));
+		int number = Integer.parseInt(args[0]);
+		double denominator = 1;
+		double fraction;
+		double pi=1;
+		for (int i=0; i<number; i++)
+		{
+			if (i%2==0) {
+				fraction= (1/denominator);
+				pi=fraction;
+			}
+			else
+			{
+				fraction=(1/denominator);
+				pi+= fraction;
+			}
+			denominator+=2;
 		}
-
-		System.out.println("pi according to Java: " + Math.PI);
-        System.out.printf("pi, approximated: " + sum * 4);
+		pi*=4.0;
+		System.out.println("pi according to Java: "+ Math.PI);
+		System.out.println("pi, approximated:       "+ pi);
 	}
 }
